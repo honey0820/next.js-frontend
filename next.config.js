2f9921dc0efc1624a5b56203819ebec47b6a7ef9
honeydev0820 <honeysoft0103@gmail.com>
2022-10-08 12:05:05 +0800

    chore(makefile): add docker_compose_run example

--- next.config.js
+++ next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
@@ -13,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = nextConfig;
